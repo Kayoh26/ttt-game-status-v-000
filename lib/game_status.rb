7cd833ae9b967fa0ca24caaeb3a6ee44f74fdc8c
Karen O <k.ortiz026@gmail.com>
2018-03-29 23:01:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,6 +30,6 @@ def won? (board)
     position_not_taken = position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
     
     position_1 == position_2 && position_2 == position_3 && position_not_taken
-    #binding.pry
+    
   end
 end
